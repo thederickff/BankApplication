f30a98f152974db3d3672f11da571de7ef73f7d0
derickfelix <derickfelix4321@gmail.com>
2017-09-16 20:52:37 -0400

    Clear to be rewritten

--- src/com/supermarket/resources/db/sql/create-table.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE `bmarket_store`.`products` ( 
-`id` INT NOT NULL AUTO_INCREMENT , 
-`name` VARCHAR(150) NOT NULL , 
-`unit_price` DECIMAL NOT NULL , 
-`description` TEXT NOT NULL , 
-`manufacturer` VARCHAR(100) NOT NULL , 
-`category` VARCHAR(100) NOT NULL , 
-PRIMARY KEY (`id`)
-) ENGINE = InnoDB;
