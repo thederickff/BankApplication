7a3188a28eb4bdc2e7bd00506d6ded7bc06d90f7
derickfelix <derickfelix4321@gmail.com>
2017-10-09 21:27:34 -0400

    Feat: implement OperationRepository

--- src/com/bankapplication/respository/impl/OperationRepository.java
+++ src/com/bankapplication/respository/impl/OperationRepository.java
@@ -8,6 +8,7 @@ package com.bankapplication.respository.impl;
 import com.bankapplication.model.Deposit;
 import com.bankapplication.model.Withdraw;
 import com.bankapplication.respository.IOperationRepository;
+import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
@@ -26,32 +27,143 @@ public class OperationRepository extends BaseRepository implements IOperationRep
 
     @Override
     public void deposit(int customerId, double amount) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Connection conn = connectionManager.createConnection();
+        String sql = "INSERT INTO " + depositTable + " (`customer_id`, `deposit_amount`) VALUES (?, ?)";
+        try {
+            // Prepare Statement
+            pstmt = conn.prepareStatement(sql);
+            pstmt.setInt(1, customerId);
+            pstmt.setDouble(2, amount);
+            
+            pstmt.execute();
+            pstmt.close();
+            conn.close();
+        } catch (SQLException ex) {
+            System.out.println("SQL Error: " + ex);
+        }
     }
 
     @Override
     public void withdraw(int customerId, double amount) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Connection conn = connectionManager.createConnection();
+        String sql = "INSERT INTO " + withdrawTable + " (`customer_id`, `withdraw_amount`) VALUES (?, ?)";
+        try {
+            // Prepare Statement
+            pstmt = conn.prepareStatement(sql);
+            pstmt.setInt(1, customerId);
+            pstmt.setDouble(2, amount);
+            
+            pstmt.execute();
+            pstmt.close();
+            conn.close();
+        } catch (SQLException ex) {
+            System.out.println("SQL Error: " + ex);
+        }
     }
 
     @Override
     public ArrayList<Deposit> deposits() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Connection conn = connectionManager.createConnection();
+        String sql = "SELECT * FROM " + depositTable;
+        // Intantiate only once
+        if (deposits == null) {
+            deposits = new ArrayList<>();
+        } else {
+            deposits.clear();
+        }
+        try {
+            statement = conn.createStatement();
+            rs = statement.executeQuery(sql);
+
+            while (rs.next()) {
+                deposits.add(depositMapper(rs));
+            }
+            rs.close();
+            statement.close();
+            conn.close();
+        } catch (SQLException ex) {
+            System.out.println("SQL Error: " + ex);
+        }
+        return deposits;
     }
 
     @Override
     public ArrayList<Withdraw> withdraws() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Connection conn = connectionManager.createConnection();
+        String sql = "SELECT * FROM " + depositTable;
+        // Intantiate only once
+        if (withdraws == null) {
+            withdraws = new ArrayList<>();
+        } else {
+            withdraws.clear();
+        }
+        try {
+            statement = conn.createStatement();
+            rs = statement.executeQuery(sql);
+
+            while (rs.next()) {
+                withdraws.add(withdrawMapper(rs));
+            }
+            rs.close();
+            statement.close();
+            conn.close();
+        } catch (SQLException ex) {
+            System.out.println("SQL Error: " + ex);
+        }
+        return withdraws;
     }
 
     @Override
     public ArrayList<Deposit> deposits(int customerId) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Connection conn = connectionManager.createConnection();
+        String sql = "SELECT * FROM " + depositTable + " WHERE `customer_id` = ?";
+        // Intantiate only once
+        if (deposits == null) {
+            deposits = new ArrayList<>();
+        } else {
+            deposits.clear();
+        }
+        try {
+            pstmt = conn.prepareStatement(sql);
+            rs = pstmt.executeQuery(sql);
+
+            while (rs.next()) {
+                deposits.add(depositMapper(rs));
+            }
+            rs.close();
+            statement.close();
+            conn.close();
+        } catch (SQLException ex) {
+            System.out.println("SQL Error: " + ex);
+        }
+        return deposits;
     }
 
     @Override
     public ArrayList<Withdraw> withdraws(int customerId) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Connection conn = connectionManager.createConnection();
+        String sql = "SELECT * FROM " + depositTable + " WHERE `customer_id` = ?";
+        // Intantiate only once
+        if (withdraws == null) {
+            withdraws = new ArrayList<>();
+        } else {
+            withdraws.clear();
+        }
+        try {
+            pstmt = conn.prepareStatement(sql);
+            pstmt.setInt(1, customerId);
+            rs = pstmt.executeQuery(sql);
+
+            while (rs.next()) {
+                withdraws.add(withdrawMapper(rs));
+            }
+            rs.close();
+            statement.close();
+            conn.close();
+        } catch (SQLException ex) {
+            System.out.println("SQL Error: " + ex);
+        }
+        return withdraws;
     }
 
     /**
