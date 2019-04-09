8e71392297cce2a05280bca9557f65e53c5b886c
Derick Felix <derickfelix4321@gmail.com>
2019-04-09 20:48:47 +0200

    Doc(test): include MIT License to header

--- src/test/java/com/bankapplication/controller/CustomerControllerTest.java
+++ src/test/java/com/bankapplication/controller/CustomerControllerTest.java
@@ -1,4 +1,26 @@
-
+/*
+ * The MIT License
+ *
+ * Copyright 2019 Derick Felix.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 package com.bankapplication.controller;
 
 import com.github.derickfelix.bankapplication.controllers.CustomerController;
@@ -12,40 +34,46 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 /**
- *
  * @author derickfelix
  */
 public class CustomerControllerTest {
-    
+
     private CustomerController instance;
-    public CustomerControllerTest() {
+
+    public CustomerControllerTest()
+    {
     }
-    
+
     @BeforeClass
-    public static void setUpClass() {
+    public static void setUpClass()
+    {
     }
-    
+
     @AfterClass
-    public static void tearDownClass() {
+    public static void tearDownClass()
+    {
     }
-    
+
     @Before
-    public void setUp() {
+    public void setUp()
+    {
     }
-    
+
     @After
-    public void tearDown() {
+    public void tearDown()
+    {
     }
 
     /**
      * Test of all method, of class CustomerController.
      */
     @Test
-    public void testAll() {
+    public void testAll()
+    {
         System.out.println("all");
         instance = CustomerController.getInstance();
         ArrayList<Customer> customers = instance.getCustomers();
-        
+
         for (int i = 0; i < customers.size(); i++) {
             Customer tempCustomer = customers.get(i);
             System.out.println("Account No: " + tempCustomer.getAccountNumber());
@@ -54,16 +82,17 @@ public class CustomerControllerTest {
             System.out.println("DOB: " + tempCustomer.getDob());
         }
     }
-    
+
     /**
      * Test of searchCustomer method, of class CustomerController.
      */
     @Test
-    public void testSearchCustomer() {
+    public void testSearchCustomer()
+    {
         System.out.println("Search acccount");
         instance = CustomerController.getInstance();
         Customer customer = instance.searchCustomer(2470);
         assertTrue(customer.getName() != null);
     }
-    
+
 }
