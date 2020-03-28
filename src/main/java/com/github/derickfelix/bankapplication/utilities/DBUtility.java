08736df1a651b4041afed32b44c9833247a484a6
derickfelix <derickfelix4321@gmail.com>
2020-03-28 10:23:00 -0400

    Add database seeding

--- src/main/java/com/github/derickfelix/bankapplication/utilities/DBUtility.java
+++ src/main/java/com/github/derickfelix/bankapplication/utilities/DBUtility.java
@@ -26,8 +26,10 @@ package com.github.derickfelix.bankapplication.utilities;
 import com.github.derickfelix.bankapplication.database.BankAppTemplate;
 import com.github.derickfelix.bankapplication.models.User;
 import com.github.derickfelix.bankapplication.repositories.impl.UserRepositoryImpl.UserMapper;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Optional;
+import java.util.Random;
 import org.h2.tools.Server;
 
 public class DBUtility {
@@ -73,6 +75,8 @@ public class DBUtility {
                 "id identity", "account_number varchar", "withdraw_amount decimal"
         );
         
+        // Optional
+        seedTables();
         checkDefaultUser();
     }
 
@@ -100,7 +104,152 @@ public class DBUtility {
         Optional<User> optional = template.queryForObject(sql, null, new UserMapper());
         
         if (!optional.isPresent()) {            
-            template.update("insert into users (name, username, password, role) values ('Admin', 'root', HASH('SHA256', '1234'), 'admin')", null);
+            template.update("insert into users (name, username, password, role) values ('Admin User', 'root', HASH('SHA256', 'admin'), 'Administrator')", null);
         }
+    }
+    
+    private static void seedTables()
+    {
+        if (!hasData("users")) {
+            seedUsers();
+        }
+        if (!hasData("customers")) {
+            seedCustomers();
+        }
+    }
+    
+    private static boolean hasData(String table)
+    {
+        String sql = "select count(*) as count from " + table;
+        
+        Integer count = template.queryForObject(sql, null, (ResultSet rs) -> rs.getInt("count")).orElse(0);
+        
+        return count > 0;
+    }
+    
+    private static void seedUsers()
+    {
+        StringBuilder sql = new StringBuilder("insert into users (name, username, password, role) values\n");
+        Random random = new Random();
+        String[] maleNames = maleNames();
+        String[] femaleNames = femaleNames();
+        int amount = 100;
+
+        for (int i = 0; i < amount; i++) {
+            sql.append('(');
+            boolean male = random.nextBoolean();
+            String firstName = male ? maleNames[random.nextInt(maleNames.length)] : femaleNames[random.nextInt(femaleNames.length)];
+
+            // Full name
+            sql.append("'");
+            sql.append(firstName);
+            sql.append(' ');
+            sql.append(maleNames[random.nextInt(maleNames.length)]);
+            sql.append("', '");
+            // username
+            sql.append(firstName.toLowerCase());
+            sql.append(random.nextInt(1000));
+            // password
+            sql.append("', HASH('SHA256', '123456'), 'Standard')");
+
+            if ((i + 1) < amount) {
+                sql.append(",\n");
+            }
+        }
+
+        template.update(sql.toString(), null);
+    }
+
+    private static void seedCustomers()
+    {
+        StringBuilder sql = new StringBuilder("insert into customers (name, address, sex, account_number, account_type) values\n");
+        Random random = new Random();
+        String[] maleNames = maleNames();
+        String[] femaleNames = femaleNames();
+        String[] states = states();
+        int amount = 1000;
+
+        for (int i = 0; i < amount; i++) {
+            sql.append('(');
+            boolean male = random.nextBoolean();
+            
+            // Full name
+            sql.append("'");
+            sql.append(male ? maleNames[random.nextInt(maleNames.length)] : femaleNames[random.nextInt(femaleNames.length)]);    
+            sql.append(' ');
+            sql.append(maleNames[random.nextInt(maleNames.length)]);    
+            
+            sql.append("', '");
+            // address
+            sql.append(random.nextInt(8066) + 1000);
+            sql.append(" Example St, ");
+            sql.append(states[random.nextInt(states.length)]);
+            sql.append(' ');
+            sql.append(random.nextInt(38208) + 1000);
+            
+            sql.append("', '");
+            // sex
+            sql.append(male ? 'm': 'f');
+            sql.append("', '");
+            // account number
+            sql.append(random.nextInt(175665) + 100000);
+            sql.append("-");
+            sql.append(random.nextInt(1000) + 100);
+            sql.append("', '");
+            // account type
+            sql.append(random.nextBoolean() ? "Current" : "Savings");
+            sql.append("')");
+            
+            if ((i + 1) < amount) {
+                sql.append(",\n");
+            }
+        }
+        
+        template.update(sql.toString(), null);
+    }
+    
+    private static String[] maleNames()
+    {
+        return new String[] {
+            "Jeff", "Abel", "Daron", "Lenard", "Reinaldo", "Earnest",
+            "Gilberto", "Thaddeus", "Rafael", "Leland", "Cristopher",
+            "Nickolas", "Darrick", "Jon", "Houston", "Rashad",
+            "Jason", "Archie", "Carmen", "Thurman", "Forest", "Dannie",
+            "Dick", "David", "Nick", "Stefan", "Stephen", "Mauricio",
+            "Sidney", "Pat", "Garry", "Dee", "Rosario", "Ellis",
+            "Bob", "Julio", "Chung", "Raymond", "Timmy", "Xavier",
+            "Clay", "Clark", "Kris", "Raleigh", "Neville", "Brendon",
+            "Wilford", "Zachariah", "Ollie", "Silas", "Terry", "Bennett",
+            "Asa", "Olen", "Everett", "Benjamin", "Bobbie", "Renaldo",
+            "Murray", "Josue", "Jamison", "Boyce", "Carson", "Edgardo",
+            "Kenton", "Ernesto", "Brain", "Jack", "Truman", "Les", "Antonia",
+            "Thurman", "Robby", "Kris", "Raymon", "Adam", "Damon", "Bernardo",
+            "Christoper", "Hank", "Man", "Ivory", "Markus", "Weston",
+            "Kendall", "Edmond", "Royal", "Mervin", "Art", "Moshe",
+            "Dorian", "Eugenio", "Noe", "Ronald", "Emerson", "Ambrose",
+            "Avery", "Miquel", "Ruben", "Wyatt"
+        };
+    }
+    
+    private static String[] femaleNames()
+    {
+        return new String[] {
+            "Eladia", "Phylicia", "Karey", "Lissa", "Shela", "Maryetta",
+            "Kimberely", "Estrella", "Bev", "Vallie", "Tonda", "Carline",
+            "Agripina", "Tiffani", "Yan", "Dori", "Amberly", "Noreen",
+            "Susan", "Josphine", "Garnet", "Cori", "Trina", "Lyla", "Felicia",
+            "Nikole", "Katelynn", "Laveta", "Latasha", "Inell", "Teressa",
+            "Temple", "Oma", "Marlen", "Larhonda", "Fae", "Carole", "Tess", 
+            "Zofia", "Noelia", "Lyndsay", "Odessa", "Yolanda", "Antonina", 
+            "Delorse", "Marry", "Misty", "Winnifred", "Alethia", "Marth"
+        };
+    }
+    
+    private static String[] states()
+    {
+        return new String[] {
+           "NY", "CA", "MS", "MD", "GA", "PA", "NJ", "WI", "IL", "MA",
+           "CT", "SC", "NC", "VA", "TN", "FL"
+        };
     }
 }
